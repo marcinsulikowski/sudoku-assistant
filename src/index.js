78de023f27a98e4798d308d7e869bf92ad77c9c1
Marcin Sulikowski <marcin.k.sulikowski@gmail.com>
2020-05-24 14:28:11 +0200

    Transform all the boilerplace into something

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.square.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(row, column) {
+    return <Square
+      square={this.props.squares[row][column]}
+      onClick={() => this.props.setValue(row, column, 8)}
+    />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="box">
+          {this.renderSquare(0, 0)}
+          {this.renderSquare(0, 1)}
+          {this.renderSquare(0, 2)}
+          <br />
+          {this.renderSquare(1, 0)}
+          {this.renderSquare(1, 1)}
+          {this.renderSquare(1, 2)}
+          <br />
+          {this.renderSquare(2, 0)}
+          {this.renderSquare(2, 1)}
+          {this.renderSquare(2, 2)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "big",
+      squares: Array(9),
+    };
+
+    for (let row = 0; row < 9; row++) {
+      this.state.squares[row] = Array(9);
+      for (let column = 0; column < 9; column++) {
+        this.state.squares[row][column] = {
+          value: null,
+          centerMarks: Array(9).fill(false),
+          cornerMarks: Array(9).fill(false),
+        }
+      }
+    }
+  }
+
+  setValue(row, column, value) {
+    if (this.state.mode === "big") {
+      let newState = JSON.parse(JSON.stringify(this.state))
+      newState.squares[row][column].value = value;
+      this.setState(newState)
+    }
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={this.state.squares}
+            setValue={(row, column, value) => this.setValue(row, column, value)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
